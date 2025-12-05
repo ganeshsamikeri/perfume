3e86a01e7f64c7f8383ed96fbb1ace8a2f451b8f
Ganesh Samikeri <ganeshsamikeri@gmail.com>
2025-12-05 23:25:27 +0530

    Update API URL and product page API calls

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { API_BASE_URL } from "../config";
 
 const api = axios.create({
-  baseURL: "https://perfume-1-zm0w.onrender.com/api", 
+  baseURL: `${API_BASE_URL}/api`,
 });
 
 export default api;
